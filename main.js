56415645f1111b616ccee733f61f42ce7e1ee340
Moomed-Hamed <mohamedamre2000@gmail.com>
2021-01-01 19:06:48 -0500

    initial source commit

---
+++ main.js
@@ -0,0 +1,7 @@
+
+document.getElementById("test").style.color = 'purple';
+
+function buttonFunc() {
+    //document.body.style.backgroundColor = "red";
+    //document.getElementsByClassName("test").color = 'white';
+}

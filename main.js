3a41580a12e21ffd3a8dd9b32efe02ac7f2e45ca
Moomed-Hamed <mohamedamre2000@gmail.com>
2021-01-01 19:06:48 -0500

    website v1

---
+++ main.js
@@ -0,0 +1,7 @@
+
+//document.getElementById("test").style.color = 'aquamarine';
+
+function buttonFunc() {
+    document.body.style.backgroundColor = "red";
+    //document.getElementsByClassName("test").color = 'white';
+}

3bda6eed68375e45b05812b5c556cbc1ed1aabf1
Moomed-Hamed <mohamedamre2000@gmail.com>
2022-09-12 18:36:17 -0400

    restarting

--- main.js
+++ main.js
@@ -5,3 +5,20 @@ function buttonFunc() {
     document.body.style.backgroundColor = "red";
     //document.getElementsByClassName("test").color = 'white';
 }
+
+// Navigation Menu
+/*
+var nav_menu = `
+  <ul>
+    <li onclick="javascript:location.href='./index.html'"    > Home     </li>
+    <li onclick="javascript:location.href='./projects.html'" > Projects </li>
+    <li onclick="javascript:location.href='./contact.html'"  > Contact  </li>
+    <li onclick="javascript:location.href='./resume.html'"   > Resume   </li>
+  </ul>`
+
+var tag = document.createElement("nav");
+var text = document.createTextNode(nav_menu);
+tag.appendChild(text);
+//var element = document.getElementById("body");
+document.body.appendChild(tag);
+*/
